dc5b9ad00aae7b4c8bcb729669daade97af0bab3
Pramod Kumar Yadav <72774410+iampramodyadav@users.noreply.github.com>
2025-03-09 22:22:29 +0100

    Update __init__.py

--- vector_transform/__init__.py
+++ vector_transform/__init__.py
@@ -1,5 +1,5 @@
 # vector_transform/__init__.py
-from .decor import *    # Import all functions from decor.py
+from .file_decor import *    # Import all functions from decor.py
 from .vector3d import * # Import all functions from vector3d.py
 
 __version__ = "0.1.0"   # Define your package version
