45f1b1f3d6427a406f7304b44d72b76d6d39129c
Pramod Kumar Yadav <72774410+iampramodyadav@users.noreply.github.com>
2025-03-09 22:33:50 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,9 +11,20 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/iampramodyadav/vector-transform",
     packages=find_packages(),
-    install_requires=["numpy"],  # Add dependencies like ["numpy"]
+    install_requires=[
+        "numpy>=1.21.0",
+        "pandas>=1.5.0",
+        "matplotlib>=3.5.0",
+        "scipy>=1.7.0",
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
